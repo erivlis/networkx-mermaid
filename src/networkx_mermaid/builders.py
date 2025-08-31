b17ffff0c2d168011127834b39403dc33bb59162
Eran Rivlis <eran.rivlis@yahoo.com>
2025-08-31 15:59:00 +0300

    refactor: replace unique_strings with MinifyingMapper for ID generation in DiagramBuilder

--- src/networkx_mermaid/builders.py
+++ src/networkx_mermaid/builders.py
@@ -1,8 +1,7 @@
-from collections import defaultdict
 from typing import Any
 
 import networkx as nx
-from mappingtools.operators import unique_strings
+from mappingtools.collectors import MinifyingMapper
 
 from .models import DiagramNodeShape, DiagramOrientation
 from .typing import MermaidDiagram
@@ -66,11 +65,11 @@ class DiagramBuilder:
         Initialize the DiagramBuilder.
 
         Args:
-            orientation: The orientation of the graph (default: LEFT_RIGHT).
-            node_shape: The shape of the nodes (default: DEFAULT).
-            layout: the layout to use (default: 'dagre')
-            look: the look to use (default: 'neo')
-            theme: the theme to use (default: 'neutral')
+            orientation: DiagramOrientation - The orientation of the graph (default: LEFT_RIGHT).
+            node_shape: DiagramNodeShape - The shape of the nodes (default: DiagramNodeShape.DEFAULT).
+            layout: str - the layout to use (default: 'dagre')
+            look: str - the look to use (default: 'neo')
+            theme: str - the theme to use (default: 'neutral')
         """
         self.orientation = orientation
         self.node_shape = node_shape
@@ -83,21 +82,8 @@ class DiagramBuilder:
         if not isinstance(node_shape, DiagramNodeShape):
             raise TypeError("node_shape must be a valid NodeShape enum")
 
-    def build(self, graph: nx.Graph, title: str | None = None, with_edge_labels: bool = True) -> MermaidDiagram:
-        """
-        Materialize a graph as a Mermaid flowchart.
-
-        Args:
-            graph: The NetworkX graph to convert.
-            title: The title of the graph (default: None).
-                   If None, the graph name will be used if available.
-                   Supplying and empty string will remove the title.
-            with_edge_labels: Whether to include edge labels (default: True).
-
-        Returns:
-            A string representation of the graph as a Mermaid graph.
-        """
-        config = (
+    def _diagram_config(self, graph, title: str | None = None) -> str:
+        return (
             f"---\n"
             f"{_graph_title(graph, title)}"
             f"config:\n"
@@ -107,23 +93,31 @@ class DiagramBuilder:
             f"---\n"
         )
 
+    def build(self, graph: nx.Graph, title: str | None = None, with_edge_labels: bool = True) -> MermaidDiagram:
+        """
+        Materialize a graph as a Mermaid flowchart.
+
+        Args:
+            graph: nx.Graph - The NetworkX graph to convert.
+            title: str - The title of the graph (default: None).
+                   If None, the graph name will be used if available.
+                   Supplying and empty string will remove the title.
+            with_edge_labels: bool - Whether to include edge labels (default: True).
+
+        Returns:
+            A string representation of the graph as a Mermaid graph.
+        """
+        config = self._diagram_config(graph, title)
+
         bra, ket = self.node_shape.value
 
-        _us = unique_strings()
-
-        def _next_id():
-            return next(_us)
-
-        _minified_ids = defaultdict(_next_id)
-
-        def _minify_id(key):
-            return _minified_ids[key]
+        minifier = MinifyingMapper()
 
         nodes = "\n".join(
-            f"{_minify_id(u)}{bra}{d.get('label', u)}{ket}{_node_style(_minify_id(u), d)}" for u, d in
+            f"{minifier.get(u)}{bra}{d.get('label', u)}{ket}{_node_style(minifier.get(u), d)}" for u, d in
             graph.nodes.data())
 
-        _edges = ((_minify_id(u), _minify_id(v), d) for u, v, d in graph.edges.data())
+        _edges = ((minifier.get(u), minifier.get(v), d) for u, v, d in graph.edges.data())
         edges = "\n".join(f"{u} -->{_edge_label(d) if with_edge_labels else ''} {v}" for u, v, d in _edges)
 
         return (
