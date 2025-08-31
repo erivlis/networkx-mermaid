1853ac157a6124bd10479e2bb78c2c364146a1f0
Eran Rivlis <eran.rivlis@yahoo.com>
2025-08-31 16:05:55 +0300

    refactor: replace StrEnum with Enum for diagram layout, look, theme, and orientation for Python 3.10 compatibility

--- src/networkx_mermaid/models.py
+++ src/networkx_mermaid/models.py
@@ -1,23 +1,23 @@
-from enum import Enum, StrEnum
+from enum import Enum
 
 
-class DiagramLayout(StrEnum):
+class DiagramLayout(Enum):
     """Layout of a Mermaid diagram"""
     DAGRE = 'dagre'
     ELK = 'elk'
 
 
-class DiagramLook(StrEnum):
+class DiagramLook(Enum):
     """Look of a Mermaid diagram"""
     NEO = 'neo'
 
 
-class DiagramTheme(StrEnum):
+class DiagramTheme(Enum):
     """Theme of a Mermaid diagram"""
     NEUTRAL = 'neutral'
 
 
-class DiagramOrientation(StrEnum):
+class DiagramOrientation(Enum):
     """Orientation of a Mermaid graph"""
     TOP_DOWN = 'TD'
     BOTTOM_UP = 'BT'
